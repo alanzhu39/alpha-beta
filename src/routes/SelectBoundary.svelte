b2a62fb4a2ef764d017a2eb7fe49ee5b7564f5c2
Alan Zhu <alan.zhu39@gmail.com>
2024-06-15 19:58:27 -0700

    testing canvas sizing

--- src/routes/SelectBoundary.svelte
+++ src/routes/SelectBoundary.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
 	export let videoSrc: string;
+
+	let videoRef: HTMLVideoElement | null = null;
+	let canvasRef: HTMLCanvasElement | null = null;
+
+	$: {
+		if (videoRef && canvasRef) {
+			console.log(videoRef);
+			console.log(videoRef.getBoundingClientRect());
+			canvasRef.width = videoRef.offsetWidth;
+			canvasRef.height = videoRef.offsetHeight;
+		}
+	}
 </script>
 
 <div class="container">
 	<!-- TODO: back button -->
-	<video class="user-video" controls muted>
+	<video class="user-video" muted bind:this={videoRef}>
 		<source src={videoSrc} type="video/mp4" />
 	</video>
-	<canvas class="video-canvas" id="video-canvas"></canvas>
+	<canvas class="video-canvas" id="video-canvas" bind:this={canvasRef}></canvas>
 	<!-- TODO: point selection canvas -->
 	<!-- TODO: bottom text and button -->
 	<p>Bottom text</p>
@@ -28,7 +40,9 @@
 
 	.video-canvas {
 		position: absolute;
+		z-index: 1;
 		top: 0;
-		left: 0;
+		left: 50%;
+		transform: translateX(-50%);
 	}
 </style>
